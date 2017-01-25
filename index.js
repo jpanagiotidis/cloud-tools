4549a971531556217452140648e52c72eb5be4fb
Jonathan <panagiotidisjonathan@gmail.com>
2017-01-25 15:34:22 +0000

    small fix

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const getId = require('./source/id').getId;
 const config = require('./source/config/utils.js');
+const dateUtils = require('./source/date-utils');
 const aws = require('./source/aws');
 
 module.exports = {
   getId,
   config,
+  dateUtils,
   aws,
 };
