42870db756625f7d48008437b582ded73764bc81
Jonathan Panagiotidis <jpanagiotidis@Jonathans-MacBook-Pro.local>
2017-02-09 11:22:15 +0000

    added some debug logs

--- source/id/index.js
+++ source/id/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const getEC2Id = require('../aws').ec2.getInstanceId;
+const debug = require('../../debug');
 const DateObj = require('../date-utils').DateObj;
 const Chance = require('chance');
 
 let id;
+const debugName = 'CloudID';
 
 function createId(name) {
   if (['development', 'dev'].indexOf(process.env.NODE_ENV) !== -1) {
+    debug(debugName, 'createId: generating fresh fake id');
     const ch = new Chance();
     return Promise.resolve(`${name}-${ch.string({
       length: 10,
@@ -15,9 +18,11 @@ function createId(name) {
     })}`);
   }
   return new Promise((resolve, reject) => {
+    debug(debugName, 'generating fresh aws id');
     getEC2Id()
     .then((ec2Id) => {
       const ts = (new DateObj()).getTimestamp();
+      debug(debugName, 'createId: completed aws id');
       resolve(`${name}-${ec2Id}-${process.pid}-${ts}`);
     })
     .catch(reject);
@@ -26,6 +31,7 @@ function createId(name) {
 
 function getId(name) {
   if (id) {
+    debug(debugName, 'getId: returning cached ID');
     return Promise.resolve(id);
   }
 
