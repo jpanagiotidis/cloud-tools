94b77f670a1719be8af76d2116c8b498a197f85e
Jonathan Panagiotidis <jpanagiotidis@Jonathans-MacBook-Pro.local>
2017-02-26 16:37:10 +0200

    bug fix on id

--- source/id/index.js
+++ source/id/index.js
@@ -2,28 +2,27 @@
 
 const getEC2Id = require('../aws').ec2.getInstanceId;
 const debug = require('../debug');
-const DateObj = require('../date-utils').DateObj;
+const getTimestamp = require('../date-utils').getTimestamp;
 
 const debugName = 'CloudID';
 let id;
 
 function getId(name) {
-  if (id) {
-    debug(debugName, 'getId: returning cached id');
-    return Promise.resolve(id);
+  if (!id) {
+    debug(debugName, 'getId: generating fresh aws id');
+    id = new Promise((resolve, reject) => {
+      getEC2Id()
+      .then((ec2Id) => {
+        const ts = getTimestamp();
+        id = `${name}-${ec2Id}-${process.pid}-${ts}`;
+        debug(debugName, `getId: returning id ${id}`);
+        resolve(id);
+      })
+      .catch(reject);
+    });
   }
 
-  debug(debugName, 'getId: generating fresh aws id');
-  return new Promise((resolve, reject) => {
-    getEC2Id()
-    .then((ec2Id) => {
-      const ts = (new DateObj()).getTimestamp();
-      id = `${name}-${ec2Id}-${process.pid}-${ts}`;
-      debug(debugName, `getId: returning id ${id}`);
-      resolve(id);
-    })
-    .catch(reject);
-  });
+  return id;
 }
 
 module.exports = {
